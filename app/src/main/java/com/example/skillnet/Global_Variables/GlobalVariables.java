980d7903b57fbe460f969f3322cf5011021bfdf9
MalkiAmasha <malkiamasha0316@gmail.com>
2024-06-18 11:29:54 +0530

    update settingFragment

---
+++ app/src/main/java/com/example/skillnet/Global_Variables/GlobalVariables.java
@@ -0,0 +1,16 @@
+package com.example.skillnet.Global_Variables;
+
+public class GlobalVariables {
+
+    // Global variables to indicate the account type
+    public static boolean isWorker = true;  // Default to worker
+    public static boolean isNotWorker = false;
+
+    // Other global variables you might have
+    public static boolean addAccount = false;
+    public static boolean switchAccount = false;
+
+
+}
+
+
